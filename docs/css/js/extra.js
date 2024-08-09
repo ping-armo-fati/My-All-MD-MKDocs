117e7d13b2a66b43ecc17990eae20aab38ae5e61
ping-armo-fati <630585687@qq.com>
2024-08-09 18:29:22 +0800

    theme

--- docs/css/js/extra.js
+++ docs/css/js/extra.js
@@ -4,11 +4,19 @@ document.addEventListener('DOMContentLoaded', function() {
         const button = document.createElement('button');
         button.innerText = 'copy';
         button.className = 'copy-button';
+
         button.onclick = () => {
             navigator.clipboard.writeText(codeBlock.innerText).then(() => {
                 alert('已复制到剪贴板！');
             });
         };
+
+        // 将按钮添加到代码块的父元素
         codeBlock.parentNode.insertBefore(button, codeBlock);
+
+        // 设置按钮的样式
+        button.style.position = 'absolute';
+        button.style.top = '10px'; // 距离顶部
+        button.style.right = '10px'; // 距离右侧
     });
 });
